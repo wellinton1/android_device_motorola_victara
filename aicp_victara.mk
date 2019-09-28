77173930753a0e8bb7cfbd3e1346c1d4a0b649a7
jro1979oliver <jroliveira.oliveira301@gmail.com>
2019-09-28 20:06:54 +0000

    AICP-fy

--- aicp_victara.mk
+++ aicp_victara.mk
@@ -1,15 +1,15 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Aosp stuff.
-$(call inherit-product, vendor/aosp/config/common.mk)
+# Inherit some common Aicp stuff.
+$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
 
 # Inherit from victara device
 $(call inherit-product, device/motorola/victara/device.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := victara
-PRODUCT_NAME := aosp_victara
+PRODUCT_NAME := aicp_victara
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := victara
 PRODUCT_RELEASE_NAME := MOTO X (2014)
@@ -19,11 +19,3 @@ PRODUCT_MANUFACTURER := motorola
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
-
-# Use Jelly
-TARGET_USE_JELLY := true
-
-# Use Gapps
-#WITH_GAPPS := true
-#TARGET_GAPPS_ARCH := arm
-#IS_PHONE := true
