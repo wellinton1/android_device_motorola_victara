df9493a44f11c8daf1a789f24b69f21139435435
jro1979oliver <jroliveira.oliveira301@gmail.com>
2019-11-02 22:12:55 +0000

    victara: Build clearkey HIDL 1.2 service.

--- device.mk
+++ device.mk
@@ -163,7 +163,9 @@ PRODUCT_PACKAGES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service
+    android.hardware.drm@1.0-service\
+    android.hardware.drm@1.0-service \
+    android.hardware.drm@1.2-service.clearkey
 
 # Display
 PRODUCT_PACKAGES += \
