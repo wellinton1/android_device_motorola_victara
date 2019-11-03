a053d0fbb7163045e982c0074e4c3d8489d14eed
Jeferson Rodrigo de Oliveira <jroliveira.oliveira301@gmail.com>
2019-11-03 15:54:38 -0300

    victara: add prebuilt camera

--- device.mk
+++ device.mk
@@ -144,7 +144,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     camera.device@1.0-impl \
-    Snap \
     libshim_camera \
     libshims_sensorlistener \
     camera.msm8974
@@ -153,6 +152,12 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/hdrhax:system/vendor/etc/hdrhax \
     $(LOCAL_PATH)/configs/external_camera_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/external_camera_config.xml
 
+# Google Camera
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/permissions/privapp-permissions-gcam.xml:system/product/etc/permissions/privapp-permissions-gcam.xml
+
+PRODUCT_PACKAGES += \
+    GoogleCamera
 
 # CRDA
 PRODUCT_PACKAGES += \
